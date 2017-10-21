34b1c121c229effbb3db97b3b14dccf2a5848473
James Croft <jamz_c@hotmail.co.uk>
2017-10-21 17:48:04 +0100

    Added a testing playground application for Android

---
+++ MADE.Playground.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MADE.Playground.Android
+{
+    [Activity(Label = "MADE.Playground.Android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
