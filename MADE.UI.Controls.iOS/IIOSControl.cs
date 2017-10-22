f99a7538c586d5ddd63804c017e8fefde2a45acc
James Croft <jamz_c@hotmail.co.uk>
2017-10-22 21:52:31 +0100

    Added control concept blindly. Needs testing on the Mac.

---
+++ MADE.UI.Controls.iOS/IIOSControl.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IIOSControl.cs" company="MADE Apps">
+//   Copyright (c) MADE Apps.
+// </copyright>
+// <summary>
+//   Defines an interface for iOS UI elements that use a template to define their appearance when rendered.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MADE.UI.Controls
+{
+    using System.ComponentModel;
+
+    using UIKit;
+
+    /// <summary>
+    /// Defines an interface for iOS UI elements that use a template to define their appearance when rendered.
+    /// </summary>
+    public interface IIOSControl : IControl, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Gets the name of the nib to load.
+        /// </summary>
+        string NibName { get; }
+
+        /// <summary>
+        /// Gets or sets the root view.
+        /// </summary>
+        UIView RootView { get; set; }
+
+        /// <summary>
+        /// Loads the relevant control template so that it's parts can be referenced.
+        /// </summary>
+        void OnApplyTemplate();
+
+        /// <summary>
+        /// Cleans up the designer outlets.
+        /// </summary>
+        void ReleaseDesignerOutlets();
+    }
+}
