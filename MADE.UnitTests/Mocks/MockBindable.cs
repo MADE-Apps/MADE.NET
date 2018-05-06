d6dcb4e567be2c11d66484f062f7a4a47121dd0c
James Croft <jamz_c@hotmail.co.uk>
2018-05-06 09:40:57 +0100

    Added Bindable class to Mvvm project for creating bindable objects, incl. basic unit tests to verify execution.
    Also verified that docfx can generate the HTML

---
+++ MADE.UnitTests/Mocks/MockBindable.cs
@@ -0,0 +1,15 @@
+﻿namespace MADE.UnitTests.Mocks
+{
+    using MADE.App.Mvvm;
+
+    public class MockBindable : Bindable
+    {
+        private bool bindableBool;
+
+        public bool BindableBool
+        {
+            get => this.bindableBool;
+            set => this.Set(() => this.BindableBool, ref this.bindableBool, value);
+        }
+    }
+}
