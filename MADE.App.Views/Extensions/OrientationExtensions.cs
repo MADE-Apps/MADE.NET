603b57bca9aab34fe52a5ee92c068ce4cab0ce64
James Croft <jamz_c@hotmail.co.uk>
2018-05-23 07:42:42 +0100

    Added Thickness and Orientation from previous MADE library. Includes extensions for orientation

---
+++ MADE.App.Views/Extensions/OrientationExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace MADE.App.Views.Extensions
+{
+    public static class OrientationExtensions
+    {
+#if __ANDROID__
+        /// <summary>
+        /// Sets the orientation property of the given LinearLayout by the given Orientation option.
+        /// </summary>
+        /// <param name="layout">
+        /// The linear layout to update orientation.
+        /// </param>
+        /// <param name="orientation">
+        /// The orientation to update with.
+        /// </param>
+        public static void SetOrientation(
+            this Android.Widget.LinearLayout layout,
+            MADE.App.Views.Layout.Orientation orientation)
+        {
+            if (layout == null)
+            {
+                return;
+            }
+
+            layout.Orientation = orientation == MADE.App.Views.Layout.Orientation.Vertical
+                                     ? Android.Widget.Orientation.Vertical
+                                     : Android.Widget.Orientation.Horizontal;
+
+            layout.Invalidate();
+        }
+#endif
+    }
+}
