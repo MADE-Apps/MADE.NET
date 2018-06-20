bbcc15f06c09e9fb47ad39c39606eec1a3910964
James Croft <jamz_c@hotmail.co.uk>
2018-06-20 15:26:31 +0100

    Added Networking library with ported code from WinUX

---
+++ MADE.App.Networking/Requests/Json/JsonPutNetworkRequest.cs
@@ -0,0 +1,192 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="JsonPutNetworkRequest.cs" company="MADE Apps">
+//   Copyright (c) MADE Apps.
+// </copyright>
+// <summary>
+//   Defines a network request for a PUT call with a JSON response.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MADE.App.Networking.Requests.Json
+{
+#if WINDOWS_UWP
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Windows.Storage.Streams;
+    using Windows.Web.Http;
+
+    using Newtonsoft.Json;
+
+#else
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
+
+#endif
+
+    /// <summary>
+    /// Defines a network request for a PUT call with a JSON response.
+    /// </summary>
+    public sealed class JsonPutNetworkRequest : NetworkRequest
+    {
+        private readonly HttpClient client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonPutNetworkRequest"/> class.
+        /// </summary>
+        /// <param name="client">
+        /// The <see cref="HttpClient"/> for executing the request.
+        /// </param>
+        /// <param name="url">
+        /// The URL for the request.
+        /// </param>
+        public JsonPutNetworkRequest(HttpClient client, string url)
+            : this(client, url, null, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonPutNetworkRequest"/> class.
+        /// </summary>
+        /// <param name="client">
+        /// The <see cref="HttpClient"/> for executing the request.
+        /// </param>
+        /// <param name="url">
+        /// The URL for the request.
+        /// </param>
+        /// <param name="jsonData">
+        /// The JSON data to put.
+        /// </param>
+        public JsonPutNetworkRequest(HttpClient client, string url, string jsonData)
+            : this(client, url, jsonData, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonPutNetworkRequest"/> class.
+        /// </summary>
+        /// <param name="client">
+        /// The <see cref="HttpClient"/> for executing the request.
+        /// </param>
+        /// <param name="url">
+        /// The URL for the request.
+        /// </param>
+        /// <param name="jsonData">
+        /// The JSON data to put.
+        /// </param>
+        /// <param name="headers">
+        /// The additional headers.
+        /// </param>
+        public JsonPutNetworkRequest(HttpClient client, string url, string jsonData, Dictionary<string, string> headers)
+            : base(url, headers)
+        {
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+            this.Data = jsonData;
+        }
+
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        public string Data { get; set; }
+
+        /// <summary>
+        /// Executes the network request.
+        /// </summary>
+        /// <param name="cts">
+        /// The cancellation token source.
+        /// </param>
+        /// <typeparam name="TResponse">
+        /// The type of object returned from the request.
+        /// </typeparam>
+        /// <returns>
+        /// Returns the response of the request as the specified type.
+        /// </returns>
+        public override async Task<TResponse> ExecuteAsync<TResponse>(CancellationTokenSource cts = null)
+        {
+            string json = await this.GetJsonResponse(cts);
+            return JsonConvert.DeserializeObject<TResponse>(json);
+        }
+
+        /// <inheritdoc />
+        public override async Task<object> ExecuteAsync(Type expectedResponse, CancellationTokenSource cts = null)
+        {
+            string json = await this.GetJsonResponse(cts);
+            return JsonConvert.DeserializeObject(json, expectedResponse);
+        }
+
+        private async Task<string> GetJsonResponse(CancellationTokenSource cts = null)
+        {
+            if (this.client == null)
+            {
+                throw new InvalidOperationException(
+                    "No HttpClient has been specified for executing the network request.");
+            }
+
+            if (string.IsNullOrWhiteSpace(this.Url))
+            {
+                throw new InvalidOperationException("No URL has been specified for executing the network request.");
+            }
+
+            Uri uri = new Uri(this.Url);
+
+#if WINDOWS_UWP
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, uri)
+                              {
+                                  Content =
+                                      new HttpStringContent(
+                                          this.Data,
+                                          UnicodeEncoding.Utf8,
+                                          "application/json")
+                              };
+#else
+            HttpRequestMessage request =
+                new HttpRequestMessage(HttpMethod.Put, uri)
+                    {
+                        Content = new StringContent(
+                            this.Data,
+                            Encoding.UTF8,
+                            "application/json")
+                    };
+#endif
+
+            if (this.Headers != null)
+            {
+                foreach (KeyValuePair<string, string> header in this.Headers)
+                {
+                    request.Headers.Add(header.Key, header.Value);
+                }
+            }
+
+#if WINDOWS_UWP
+            HttpResponseMessage response = cts == null
+                                               ? await this.client.SendRequestAsync(
+                                                     request,
+                                                     HttpCompletionOption.ResponseHeadersRead)
+                                               : await this.client.SendRequestAsync(
+                                                     request,
+                                                     HttpCompletionOption.ResponseHeadersRead).AsTask(cts.Token);
+#else
+            HttpResponseMessage response = cts == null
+                                               ? await this.client.SendAsync(
+                                                     request,
+                                                     HttpCompletionOption.ResponseHeadersRead)
+                                               : await this.client.SendAsync(
+                                                     request,
+                                                     HttpCompletionOption.ResponseHeadersRead,
+                                                     cts.Token);
+#endif
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ MADE.App.Networking/Requests/NetworkRequest.cs
@@ -0,0 +1,92 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NetworkRequest.cs" company="MADE Apps">
+//   Copyright (c) MADE Apps.
+// </copyright>
+// <summary>
+//   Defines the model for a network request.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MADE.App.Networking.Requests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines the model for a network request.
+    /// </summary>
+    public abstract class NetworkRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NetworkRequest"/> class.
+        /// </summary>
+        /// <param name="url">
+        /// The URL for the request.
+        /// </param>
+        protected NetworkRequest(string url)
+            : this(url, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NetworkRequest"/> class.
+        /// </summary>
+        /// <param name="url">
+        /// The URL for the request.
+        /// </param>
+        /// <param name="headers">
+        /// Additional headers for the request.
+        /// </param>
+        protected NetworkRequest(string url, Dictionary<string, string> headers)
+        {
+            this.Identifier = Guid.NewGuid();
+            this.Url = url;
+            this.Headers = headers ?? new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Gets the identifier for the request.
+        /// </summary>
+        public Guid Identifier { get; }
+
+        /// <summary>
+        /// Gets or sets the URL for the request.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets the headers for the request.
+        /// </summary>
+        public Dictionary<string, string> Headers { get; }
+
+        /// <summary>
+        /// Executes the network request.
+        /// </summary>
+        /// <param name="cts">
+        /// The cancellation token source.
+        /// </param>
+        /// <typeparam name="TResponse">
+        /// The type of object returned from the request.
+        /// </typeparam>
+        /// <returns>
+        /// Returns the response of the request as the specified type.
+        /// </returns>
+        public abstract Task<TResponse> ExecuteAsync<TResponse>(CancellationTokenSource cts = null);
+
+        /// <summary>
+        /// Executes the network request.
+        /// </summary>
+        /// <param name="expectedResponse">
+        /// The type expected by the response of the request.
+        /// </param>
+        /// <param name="cts">
+        /// The cancellation token source.
+        /// </param>
+        /// <returns>
+        /// Returns the response of the request as an object.
+        /// </returns>
+        public abstract Task<object> ExecuteAsync(Type expectedResponse, CancellationTokenSource cts = null);
+    }
+}
