125cbb18edb0d5e90ea44a1feae960221241c0ed
James Croft <jamz_c@hotmail.co.uk>
2018-07-04 22:45:08 +0100

    Created interface for providing app diagnostic handling.
    Added implementations of a simple event logger for Windows (UWP) and a common one for all other platforms incl. netstandard.

---
+++ MADE.App.Diagnostics/EventLogger.Common.cs
@@ -0,0 +1,193 @@
+﻿#if !WINDOWS_UWP
+namespace MADE.App.Diagnostics
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class EventLogger : IEventLogger
+    {
+        private const string LogFormat = "{0:G}\tLevel: {1}\tId: {2}\tMessage: '{3}'";
+
+        private readonly SemaphoreSlim fileSemaphore = new SemaphoreSlim(1, 1);
+
+        /// <summary>
+        /// Gets or sets the path to where the log exists.
+        /// </summary>
+        public string LogPath { get; set; }
+
+        /// <summary>
+        /// Writes a debug information message to the event log when in DEBUG mode.
+        /// </summary>
+        /// <param name="message">
+        /// The message to write out.
+        /// </param>
+        public async void WriteDebug(string message)
+        {
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                return;
+            }
+
+            string log = string.Format(LogFormat, DateTime.Now, "Debug", Guid.NewGuid(), message);
+            await this.WriteToFileAsync(log);
+        }
+
+        /// <summary>
+        /// Writes a generic information message to the event log.
+        /// </summary>
+        /// <param name="message">
+        /// The message to write out.
+        /// </param>
+        public async void WriteInfo(string message)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debug.WriteLine(message);
+            }
+
+            string log = string.Format(LogFormat, DateTime.Now, "Debug", Guid.NewGuid(), message);
+            await this.WriteToFileAsync(log);
+        }
+
+        /// <summary>
+        /// Writes a warning message to the event log.
+        /// </summary>
+        /// <param name="message">
+        /// The message to write out.
+        /// </param>
+        public async void WriteWarning(string message)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debug.WriteLine(message);
+            }
+
+            string log = string.Format(LogFormat, DateTime.Now, "Debug", Guid.NewGuid(), message);
+            await this.WriteToFileAsync(log);
+        }
+
+        /// <summary>
+        /// Writes an error message to the event log.
+        /// </summary>
+        /// <param name="message">
+        /// The message to write out.
+        /// </param>
+        public async void WriteError(string message)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debug.WriteLine(message);
+            }
+
+            string log = string.Format(LogFormat, DateTime.Now, "Debug", Guid.NewGuid(), message);
+            await this.WriteToFileAsync(log);
+        }
+
+        /// <summary>
+        /// Writes a critical error message to the event log.
+        /// </summary>
+        /// <param name="message">
+        /// The message to write out.
+        /// </param>
+        public async void WriteCritical(string message)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debug.WriteLine(message);
+            }
+
+            string log = string.Format(LogFormat, DateTime.Now, "Debug", Guid.NewGuid(), message);
+            await this.WriteToFileAsync(log);
+        }
+
+        private async Task WriteToFileAsync(string line)
+        {
+            await this.fileSemaphore.WaitAsync();
+
+            if (string.IsNullOrWhiteSpace(this.LogPath))
+            {
+                this.SetupLogFile();
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.LogPath))
+            {
+                try
+                {
+                    using (StreamWriter sw = File.AppendText(this.LogPath))
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (System.Diagnostics.Debugger.IsAttached)
+                    {
+                        System.Diagnostics.Debug.WriteLine(
+                            $"An exception was thrown while writing to the event log file. Error: {ex}");
+                    }
+                }
+                finally
+                {
+                    this.fileSemaphore.Release();
+                }
+            }
+        }
+
+        private void SetupLogFile()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(this.LogPath))
+                {
+                    string logFileName = $"Log-{DateTime.Now:yyyyMMdd}.txt";
+
+                    string logFileFolderPath = string.Empty;
+
+#if __ANDROID__
+                    string appFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string logsFolderPath = Path.Combine(appFolderPath, "Logs");
+#elif __IOS__
+                    string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string appFolderPath = Path.Combine(documentsPath, "..", "Library");
+                    string logsFolderPath = Path.Combine(appFolderPath, "Logs");
+#else
+                    string appFolderPath = AppDomain.CurrentDomain.BaseDirectory;
+                    string logsFolderPath = Path.Combine(appFolderPath, "Logs");
+#endif
+
+                    if (!string.IsNullOrWhiteSpace(logsFolderPath))
+                    {
+                        if (!Directory.Exists(logsFolderPath))
+                        {
+                            Directory.CreateDirectory(logsFolderPath);
+                        }
+
+                        logFileFolderPath = Path.Combine(logsFolderPath, logFileName);
+                        if (!File.Exists(logFileFolderPath))
+                        {
+                            File.Create(logFileFolderPath);
+                        }
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(logFileFolderPath) && File.Exists(logFileFolderPath))
+                    {
+                        this.LogPath = logFileFolderPath;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (System.Diagnostics.Debugger.IsAttached)
+                {
+                    System.Diagnostics.Debug.WriteLine(
+                        $"An exception was thrown while setting up the event log file. Error: {ex}");
+
+                    throw;
+                }
+            }
+        }
+    }
+}
+#endif
---
+++ MADE.App.Diagnostics/IAppDiagnostics.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IAppDiagnostics.cs" company="MADE Apps">
+//   Copyright (c) MADE Apps.
+// </copyright>
+// <summary>
+//   Defines an interface for handling application diagnostics.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MADE.App.Diagnostics
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines an interface for handling application diagnostics.
+    /// </summary>
+    public interface IAppDiagnostics
+    {
+        /// <summary>
+        /// Gets the string path to the file used for capturing application diagnostic messages.
+        /// </summary>
+        string DiagnosticsFilePath { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether application diagnostic messages are being recorded.
+        /// </summary>
+        bool IsRecordingDiagnostics { get; }
+
+        /// <summary>
+        /// Starts tracking and recording the application diagnostic messages.
+        /// </summary>
+        /// <returns>
+        /// An asynchronous operation.
+        /// </returns>
+        Task StartRecordingDiagnosticsAsync();
+
+        /// <summary>
+        /// Stops tracking and recording the application diagnostic messages.
+        /// </summary>
+        void StopRecordingDiagnostics();
+    }
+}
