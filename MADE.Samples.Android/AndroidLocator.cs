221b60743449bbfb1f2277b7419e44fc8838822a
James Croft <jamz_c@hotmail.co.uk>
2018-07-05 11:20:42 +0100

    Added sample to test diagnostics.

---
+++ MADE.Samples.Android/AndroidLocator.cs
@@ -0,0 +1,24 @@
+﻿namespace MADE.Samples.Android
+{
+    using CommonServiceLocator;
+
+    using GalaSoft.MvvmLight.Ioc;
+
+    using MADE.App.Diagnostics;
+    using MADE.App.Diagnostics.Logging;
+
+    public class AndroidLocator
+    {
+        static AndroidLocator()
+        {
+            if (!ServiceLocator.IsLocationProviderSet)
+            {
+                ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            }
+
+            SimpleIoc.Default.Register<IEventLogger, EventLogger>();
+            SimpleIoc.Default.Register<IAppDiagnostics>(
+                () => new AppDiagnostics(ServiceLocator.Current.GetInstance<IEventLogger>()));
+        }
+    }
+}
--- MADE.Samples.Android/MainApplication.cs
+++ MADE.Samples.Android/MainApplication.cs
@@ -2,14 +2,22 @@
 {
     using System;
 
+    using CommonServiceLocator;
+
     using global::Android.App;
     using global::Android.OS;
     using global::Android.Runtime;
+
+    using MADE.App.Diagnostics;
 
     [Application]
     public class MainApplication : Application, Application.IActivityLifecycleCallbacks
     {
         private static Locator locator;
+
+        private static AndroidLocator androidLocator;
+
+        private IAppDiagnostics appDiagnostics;
 
         public MainApplication(IntPtr handle, JniHandleOwnership transfer)
             : base(handle, transfer)
@@ -18,41 +26,57 @@
 
         public static Locator Locator => locator ?? (locator = new Locator());
 
-        public override void OnCreate()
+        public static AndroidLocator AndroidLocator => androidLocator ?? (androidLocator = new AndroidLocator());
+
+        public override async void OnCreate()
         {
             base.OnCreate();
 
-            this.RegisterActivityLifecycleCallbacks(this);
-
             locator = new Locator();
+            androidLocator = new AndroidLocator();
+
+            this.appDiagnostics = ServiceLocator.Current.GetInstance<IAppDiagnostics>();
+            if (this.appDiagnostics != null)
+            {
+                await this.appDiagnostics.StartRecordingDiagnosticsAsync();
+            }
+
+            this.RegisterActivityLifecycleCallbacks(this);
         }
 
         public void OnActivityCreated(Activity activity, Bundle savedInstanceState)
         {
+            this.appDiagnostics.EventLogger.WriteInfo("An activity was created");
         }
 
         public void OnActivityDestroyed(Activity activity)
         {
+            this.appDiagnostics.EventLogger.WriteInfo("An activity was destroyed");
         }
 
         public void OnActivityPaused(Activity activity)
         {
+            this.appDiagnostics.EventLogger.WriteInfo("An activity was paused");
         }
 
         public void OnActivityResumed(Activity activity)
         {
+            this.appDiagnostics.EventLogger.WriteInfo("An activity was resumed");
         }
 
         public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
         {
+            this.appDiagnostics.EventLogger.WriteInfo("An activity was saved");
         }
 
         public void OnActivityStarted(Activity activity)
         {
+            this.appDiagnostics.EventLogger.WriteInfo("An activity was started");
         }
 
         public void OnActivityStopped(Activity activity)
         {
+            this.appDiagnostics.EventLogger.WriteInfo("An activity was stopped");
         }
     }
 }
