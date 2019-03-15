6df37ad25f4c627c1dd6e6b1a8b6d119577d5124
James Croft <jamesmcroft@users.noreply.github.com>
2019-03-15 14:09:57 +0000

    PR for #52 - Implemented changes to event logger and AppDiagnostics (#53)
    * Added changes to support customization of the event logging folder name and file name format.
    
    * Added changes to support Java and Android exception handling as part of the AppDiagnostics.
    
    Also includes a change to allow subscribing to listening for when the exceptions are observed.

---
+++ MADE.App.Diagnostics/Exceptions/UncaughtExceptionHandler.Android.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UncaughtExceptionHandler.Android.cs" company="MADE Apps">
+//   Copyright (c) MADE Apps.
+// </copyright>
+// <summary>
+//   Defines an implementation of a Java uncaught exception handler.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+#if __ANDROID__
+namespace MADE.App.Diagnostics.Exceptions
+{
+    using System;
+
+    using Java.Lang;
+
+    using Object = Java.Lang.Object;
+
+    /// <summary>
+    /// Defines an implementation of a Java uncaught exception handler.
+    /// </summary>
+    public class UncaughtExceptionHandler : Object, Thread.IUncaughtExceptionHandler
+    {
+        private readonly Action<System.Exception> onException;
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UncaughtExceptionHandler"/> class.
+        /// </summary>
+        /// <param name="onException">The action that will be called when an exception is observed.</param>
+        public UncaughtExceptionHandler(Action<System.Exception> onException)
+        {
+            this.onException = onException;
+        }
+
+        /// <summary>
+        /// Called when an uncaught exception is observed.
+        /// </summary>
+        /// <param name="t">The thread where the exception originated from.</param>
+        /// <param name="e">The exception.</param>
+        public void UncaughtException(Thread t, Throwable e)
+        { 
+            this.onException?.Invoke(e);
+        }
+    }
+}
+#endif
