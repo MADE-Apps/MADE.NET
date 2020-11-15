57df08ab7ea023280482f7c235ac63d232eb363e
James Croft <jamz_c@hotmail.co.uk>
2020-11-15 12:47:48 +0000

    Added tests for DateTimeToStringValueConverter

--- tests/MADE.Data.Converters.Tests/Tests/DateTimeToStringValueConverterTests.cs
+++ tests/MADE.Data.Converters.Tests/Tests/DateTimeToStringValueConverterTests.cs
@@ -29,11 +29,76 @@ namespace MADE.Data.Converters.Tests.Tests
                 // Assert
                 converted.ShouldBe(expected);
             }
+
+            [TestCase("G")]
+            [TestCase("g")]
+            public void ShouldConvertToFormattedDateTimeStringWithParameter(string format)
+            {
+                // Arrange
+                var dateTime = new DateTime(2020, 11, 8, 9, 49, 0);
+                string expected = dateTime.ToString(format, CultureInfo.InvariantCulture);
+
+                var converter = new DateTimeToStringValueConverter();
+
+                // Act
+                string converted = converter.Convert(dateTime, format);
+
+                // Assert
+                converted.ShouldBe(expected);
+            }
         }
 
         public class WhenConvertingBack
         {
+            [TestCase(null)]
+            [TestCase("G")]
+            [TestCase("g")]
+            public void ShouldConvertToDateTime(string format)
+            {
+                // Arrange
+                var expected = new DateTime(2020, 11, 8, 9, 49, 0);
+                string dateTimeString = expected.ToString(format, CultureInfo.InvariantCulture);
 
+                var converter = new DateTimeToStringValueConverter();
+
+                // Act
+                DateTime converted = converter.ConvertBack(dateTimeString);
+
+                // Assert
+                converted.ShouldBe(expected);
+            }
+
+            [Test]
+            public void ShouldReturnDateTimeMinValueIfNull()
+            {
+                // Arrange
+                DateTime expected = DateTime.MinValue;
+                string dateTimeString = null;
+
+                var converter = new DateTimeToStringValueConverter();
+
+                // Act
+                DateTime converted = converter.ConvertBack(dateTimeString);
+
+                // Assert
+                converted.ShouldBe(expected);
+            }
+
+            [Test]
+            public void ShouldReturnDateTimeMinValueIfNotDate()
+            {
+                // Arrange
+                DateTime expected = DateTime.MinValue;
+                string dateTimeString = "Hello, World!";
+
+                var converter = new DateTimeToStringValueConverter();
+
+                // Act
+                DateTime converted = converter.ConvertBack(dateTimeString);
+
+                // Assert
+                converted.ShouldBe(expected);
+            }
         }
     }
 }
