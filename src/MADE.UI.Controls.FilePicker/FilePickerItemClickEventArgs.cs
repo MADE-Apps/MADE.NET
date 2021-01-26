2ac92b2ba7604fa3b3dbce6d9c0abd1becd0a273
James Croft <jamz_c@hotmail.co.uk>
2021-01-26 20:51:16 +0000

    Added feedback changes from PR

--- src/MADE.UI.Controls.FilePicker/FilePickerItemClickEventArgs.cs
+++ src/MADE.UI.Controls.FilePicker/FilePickerItemClickEventArgs.cs
@@ -1,9 +1,19 @@
+// MADE Apps licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 namespace MADE.UI.Controls
 {
     using Windows.UI.Xaml;
 
+    /// <summary>
+    /// Defines an event argument for when a file picker item is clicked from the list.
+    /// </summary>
     public class FilePickerItemClickEventArgs : RoutedEventArgs
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilePickerItemClickEventArgs"/> class with the reference to the clicked item.
+        /// </summary>
+        /// <param name="clickedItem">The reference to the clicked item.</param>
         public FilePickerItemClickEventArgs(object clickedItem)
         {
             this.ClickedItem = clickedItem;
