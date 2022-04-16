5027d692a929b2da159b53ac56eea63c75d4b3a6
James Croft <jamz_c@hotmail.co.uk>
2022-04-16 18:01:30 +0100

    Updated entity configurations

--- src/MADE.Data.EFCore/Extensions/EntityBaseExtensions.cs
+++ src/MADE.Data.EFCore/Extensions/EntityBaseExtensions.cs
@@ -9,13 +9,28 @@ namespace MADE.Data.EFCore.Extensions
     public static class EntityBaseExtensions
     {
         /// <summary>
+        /// Configures the default properties of an <typeparamref name="TEntity">entity</typeparamref>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity to configure.</typeparam>
+        /// <param name="builder">The entity type builder associated with the entity.</param>
+        /// <returns>The entity type builder.</returns>
+        public static EntityTypeBuilder<TEntity> Configure<TEntity>(this EntityTypeBuilder<TEntity> builder)
+            where TEntity : class, IEntityBase
+        {
+            builder.HasKey(e => e.Id);
+            builder.ConfigureDateProperties();
+            return builder;
+        }
+
+        /// <summary>
         /// Configures the created and updated date properties of an <typeparamref name="TEntity">entity</typeparamref>  as UTC.
         /// </summary>
         /// <typeparam name="TEntity">The type of entity to configure.</typeparam>
         /// <param name="builder">The entity type builder associated with the entity.</param>
         /// <returns>The entity type builder.</returns>
-        public static EntityTypeBuilder<TEntity> ConfigureDateProperties<TEntity>(this EntityTypeBuilder<TEntity> builder)
-            where TEntity : EntityBase
+        public static EntityTypeBuilder<TEntity> ConfigureDateProperties<TEntity>(
+            this EntityTypeBuilder<TEntity> builder)
+            where TEntity : class, IEntityBase
         {
             builder.Property(x => x.CreatedDate).IsUtc();
             builder.Property(x => x.UpdatedDate).IsUtc();
