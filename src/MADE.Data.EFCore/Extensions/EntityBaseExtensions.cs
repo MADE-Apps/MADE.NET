87e6f041f3d89a1adb1a5ba2930d266b03240070
James Croft <james.croft@razor.co.uk>
2022-08-28 12:16:26 +0100

    Updated file headers

--- src/MADE.Data.EFCore/Extensions/EntityBaseExtensions.cs
+++ src/MADE.Data.EFCore/Extensions/EntityBaseExtensions.cs
@@ -1,3 +1,6 @@
+// MADE Apps licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 namespace MADE.Data.EFCore.Extensions
 {
     using MADE.Data.EFCore.Converters;
--- src/MADE.Networking/Http/Requests/INetworkRequest.cs
+++ src/MADE.Networking/Http/Requests/INetworkRequest.cs
@@ -1,3 +1,6 @@
+// MADE Apps licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 namespace MADE.Networking.Http.Requests
 {
     using System;
